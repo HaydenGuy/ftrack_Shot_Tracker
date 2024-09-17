017afb4cb448df11a8ca544508ad4022ac17f894
hayden <haydenguyn@gmail.com>
2024-09-17 14:52:49 -0700

    Created a temporary UI with pyside6-designer and linked it to the main file

---
+++ UI/shot_tracker_ui.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'shot_tracker.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
+
+class Ui_ftrack_Shot_Tracker(object):
+    def setupUi(self, ftrack_Shot_Tracker):
+        if not ftrack_Shot_Tracker.objectName():
+            ftrack_Shot_Tracker.setObjectName(u"ftrack_Shot_Tracker")
+        ftrack_Shot_Tracker.resize(800, 600)
+        self.centralwidget = QWidget(ftrack_Shot_Tracker)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        ftrack_Shot_Tracker.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(ftrack_Shot_Tracker)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 23))
+        ftrack_Shot_Tracker.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(ftrack_Shot_Tracker)
+        self.statusbar.setObjectName(u"statusbar")
+        ftrack_Shot_Tracker.setStatusBar(self.statusbar)
+
+        self.retranslateUi(ftrack_Shot_Tracker)
+
+        QMetaObject.connectSlotsByName(ftrack_Shot_Tracker)
+    # setupUi
+
+    def retranslateUi(self, ftrack_Shot_Tracker):
+        ftrack_Shot_Tracker.setWindowTitle(QCoreApplication.translate("ftrack_Shot_Tracker", u"ftrack Shot Tracker", None))
+        self.pushButton_2.setText(QCoreApplication.translate("ftrack_Shot_Tracker", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("ftrack_Shot_Tracker", u"PushButton", None))
+    # retranslateUi
+
--- ftrack_shot_tracker.py
+++ ftrack_shot_tracker.py
@@ -2,13 +2,18 @@ import sys
 import os
 import ftrack_api
 
-from PySide6.QtWidgets import QApplication
-from UI import ftrack_Shot_Tracker
+from PySide6.QtWidgets import QApplication, QMainWindow
+from UI.shot_tracker_ui import Ui_ftrack_Shot_Tracker
 
 # Information about the session
 # session = ftrack_api.Session(server_url="",
 #                              api_user="",
 #                              api_key="")
+
+class ftrack_Shot_Tracker(QMainWindow, Ui_ftrack_Shot_Tracker):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
