a3744c7d4626cfa0397f692d17383317f546ada3
hayden <haydenguyn@gmail.com>
2024-09-18 15:01:31 -0700

    Changed the script to accept 2 arguments only. Second argument is the target project name which is checked with get_target_project. Updated comments throughout

--- ftrack_shot_tracker.py
+++ ftrack_shot_tracker.py
@@ -17,6 +17,16 @@ session = ftrack_api.Session(server_url=f"{server_url}",
                              api_user=f"{api_user}",
                              api_key=f"{api_key}")
 
+# Check if an ftrack project exists by calling its code and return the project if it does
+def get_target_project(project_code):
+    project = session.query(f"Project where name is {project_code}").first()
+
+    if not project:
+        print("Could not find target_project")
+        sys.exit(1)
+    
+    return project
+
 class ftrack_Shot_Tracker(QMainWindow, Ui_ftrack_Shot_Tracker):
     def __init__(self):
         super().__init__()
@@ -31,9 +41,18 @@ class ftrack_Shot_Tracker(QMainWindow, Ui_ftrack_Shot_Tracker):
             self.button_box.addWidget(QPushButton(f"{button}"))
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    # Print usage statement and exit if there are not two arguments
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <target_project_code>")
+        sys.exit(1)
 
+    # Call function on the target project code (argument 2)
+    get_target_project(sys.argv[1])
+
+    # Initalize app, create and show the window
+    app = QApplication()
     window = ftrack_Shot_Tracker()
     window.show()
 
-    sys.exit(app.exec_())
+    # Start application and close it when finished
+    sys.exit(app.exec())
