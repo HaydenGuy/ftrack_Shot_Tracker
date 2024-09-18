cb90a5756bcef6cbd134a8266c73495688e74378
hayden <haydenguyn@gmail.com>
2024-09-18 14:23:01 -0700

    Added placeholder button creation method

--- ftrack_shot_tracker.py
+++ ftrack_shot_tracker.py
@@ -2,7 +2,7 @@ import sys
 import os
 import ftrack_api
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 from UI.shot_tracker_ui import Ui_ftrack_Shot_Tracker
 
 # Information about the session
@@ -14,6 +14,14 @@ class ftrack_Shot_Tracker(QMainWindow, Ui_ftrack_Shot_Tracker):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        button_names = ["testing", "button", "names"]
+
+        self.button_create(button_names)
+
+    def button_create(self, button_names):
+        for button in button_names:
+            self.button_box.addWidget(QPushButton(f"{button}"))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
