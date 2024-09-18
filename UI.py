333ff396723c5ca4ccfe8b554c8eec2fa432e263
hayden <haydenguyn@gmail.com>
2024-09-18 14:23:27 -0700

    Deleted old files

--- UI.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import (QMainWindow, QHBoxLayout, QVBoxLayout)
-
-
-class ftrack_Shot_Tracker(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("ftrack Shot Tracker")
-        self.setGeometry(0, 0, 800, 600)
